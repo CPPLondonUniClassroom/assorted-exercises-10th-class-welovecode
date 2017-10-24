332b92941987ff83cb8d0d2a4fb069a5a5fc3300
Tristan Brindle <t.c.brindle@gmail.com>
2017-10-24 17:37:42 +0100

    Add exercise 1 solutions

--- exercise1/exercise1.cpp
+++ exercise1/exercise1.cpp
@@ -6,21 +6,36 @@
 //   -- if i is odd, returns false
 bool is_even(int i)
 {
-    return false;
+    return i % 2 == 0;
 }
 
 // Requirements:
 //   -- returns the number of odd numbers in the given vector
 int count_odd(const std::vector<int>& vec)
 {
-    return 0;
+    int count = 0;
+    for (int i : vec) {
+        if (!is_even(i)) {
+            ++count;
+        }
+    }
+
+    return count;
 }
 
 // Requirements:
 //   -- returns the sum of the even numbers in the vector
 int even_sum(const std::vector<int>& vec)
 {
-    return 0;
+    int sum = 0;
+
+    for (int i : vec) {
+        if (is_even(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
 }
 
 // Requirements:
@@ -28,7 +43,15 @@ int even_sum(const std::vector<int>& vec)
 //      divisible by p
 std::vector<int> copy_if_divisible_by(const std::vector<int>& vec, int p)
 {
-    return std::vector<int>{};
+    std::vector<int> output;
+
+    for (auto i : vec) {
+        if (i % p == 0) {
+            output.push_back(i);
+        }
+    }
+
+    return output;
 }
 
 // Requirements:
@@ -36,12 +59,29 @@ std::vector<int> copy_if_divisible_by(const std::vector<int>& vec, int p)
 //      zero if there are no such numbers
 int largest_div_by_three(const std::vector<int>& vec)
 {
-    return 0;
+    const auto div_three = copy_if_divisible_by(vec, 3);
+    if (div_three.empty()) {
+        return 0;
+    }
+
+    int max = std::numeric_limits<int>::min();
+    for (int i : div_three) {
+        if (i > max) {
+            max = i;
+        }
+    }
+
+    return max;
 }
 
 // Requirements:
 //   -- returns true if the elements in vec are sorted, and false otherwise
 bool is_sorted(const std::vector<int>& vec)
 {
-    return false;
+    for (int i = 1; i < vec.size(); ++i) {
+        if (vec[i] < vec[i - 1]) {
+            return false;
+        }
+    }
+    return true;
 }
