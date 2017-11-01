c5ca4f29ac62de214df1400d65788d145a2c067a
Tristan Brindle <t.c.brindle@gmail.com>
2017-11-01 13:53:34 +0000

    Fix error in CSV parsing test

--- exercise3/test/test_parse_csv.cpp
+++ exercise3/test/test_parse_csv.cpp
@@ -9,11 +9,11 @@ const std::string unquoted = R"(first,second,third,
 
 const std::string quoted = R"(first,second,third,
 1,2,3
-"4,5",6, 7)";
+"4,5",6,7)";
 
 TEST_CASE("Unquoted CSV files can be parsed as expected")
 {
-    std::istringstream ss(quoted);
+    std::istringstream ss(unquoted);
     const auto csv = parse_csv(ss);
     const auto result = std::vector<std::vector<std::string>>{
         {"first", "second", "third"},
